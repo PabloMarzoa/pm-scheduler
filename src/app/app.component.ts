df528fcf1cd54ab7d42261391e6152ec207acdaf
Pablo <pablomarzoalopez@gmail.com>
2024-06-05 08:39:14 +0200

    feat: init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SchedulerComponent} from "./components/scheduler/scheduler-component/scheduler.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SchedulerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
